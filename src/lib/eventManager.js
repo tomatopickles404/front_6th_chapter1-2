34cdd63ce3883c416bc7754c2ba23ccd27b9d334
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-16 03:39:01 +0900

    feat: setupEventListeners 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,29 @@
-// export function setupEventListeners(root) {}
+const eventHandlers = new Map();
 
-// export function addEvent(element, eventType, handler) {}
+const handleEvent = (event) => {
+  const { target, type } = event;
+  const key = `${target.id}-${type}`;
+  const eventHandler = eventHandlers.get(key);
 
-// export function removeEvent(element, eventType, handler) {}
+  if (eventHandler) {
+    eventHandler(event);
+  }
+};
+
+export function setupEventListeners(root, eventType = "click") {
+  root.addEventListener(eventType, handleEvent);
+
+  return () => {
+    root.removeEventListener(eventType, handleEvent);
+  };
+}
+
+export function addEvent(element, eventType, handler) {
+  const key = `${element.id}-${eventType}`;
+  eventHandlers.set(key, handler);
+}
+
+export function removeEvent(element, eventType) {
+  const key = `${element.id}-${eventType}`;
+  eventHandlers.delete(key);
+}
