95e4a063bf41dc8c25e2cb3935d66c237bb36c7a
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-16 03:52:16 +0900

    fix: createVNode 자식 배열 평탄화

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,9 +1,11 @@
 import { isFalsy } from "../utils/isFalsy";
 
 export function createVNode(type, props, ...children) {
+  if (isFalsy(type)) return "";
+
   return {
     type,
     props,
-    children: children.filter((child) => !isFalsy(child)).flat(),
+    children: children.filter((child) => !isFalsy(child)).flat(Infinity),
   };
 }
