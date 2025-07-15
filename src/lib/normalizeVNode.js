04d213b8434c905c6a9590f1678d5448690778bc
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-15 14:59:39 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,29 @@
+const isFalsy = (vNode) => vNode === null || typeof vNode === "undefined" || typeof vNode === "boolean";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (isFalsy(vNode)) return "";
+  const safeChildren = vNode.children ?? [];
+
+  // console.log("vNode", vNode);
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  // 함수형 컴포넌트 재귀 처리
+  if (typeof vNode.type === "function") {
+    const safeProps = vNode.props ?? {};
+    // children을 포함하여 전달
+    const propsWithChildren = { ...safeProps, children: safeChildren };
+    return normalizeVNode(vNode.type(propsWithChildren));
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.filter((child) => !isFalsy(child)).flatMap((child) => normalizeVNode(child));
+  }
+
+  return {
+    ...vNode,
+    children: safeChildren.filter((child) => !isFalsy(child)).flatMap((child) => normalizeVNode(child)),
+  };
 }
