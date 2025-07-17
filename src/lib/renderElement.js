9e2b7f40e61e18f2a00c37fb500ea0326a775e44
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-17 16:48:41 +0900

    refactor: renderElement 수정

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,31 +1,25 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
+// import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // vNode를 정규화
   const normalizedVNode = normalizeVNode(vNode);
+  // const existingElement = container.firstChild;
 
-  // 기존 DOM이 있는지 확인
-  const existingElement = container.firstChild;
-
-  if (!existingElement) {
-    // 초기 렌더링: 컨테이너 초기화 후 새로 생성
+  // if (!existingElement) {
+  const element = createElement(normalizedVNode);
+  if (element && element !== "") {
     container.innerHTML = "";
-    const element = createElement(normalizedVNode);
-    if (element && element !== "") {
-      container.appendChild(element);
-    }
-  } else {
-    // 업데이트 렌더링: diff 알고리즘 사용
-    // 기존 vNode 구조를 추정 (실제로는 이전 vNode를 저장해야 함)
-    // 현재는 간단히 기존 요소를 교체하는 방식으로 구현
-    const newElement = createElement(normalizedVNode);
-    if (newElement && newElement !== "") {
-      container.replaceChild(newElement, existingElement);
-    }
+    container.appendChild(element);
   }
+  // } else {
+  // 업데이트 렌더링: diff 알고리즘 사용
+  // updateElement(existingElement, normalizedVNode, existingElement._vNode);
+  // }
 
-  // 이벤트를 등록합니다. (컨테이너 위임)
-  setupEventListeners(container);
+  if (!container._eventListenersSetup) {
+    setupEventListeners(container);
+    container._eventListenersSetup = true;
+  }
 }
