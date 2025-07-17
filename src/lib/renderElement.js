dd49e0614456c277d579d51f7f20b76630b50863
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-18 02:44:57 +0900

    refactor: 부분리렌더링 함수 적용

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,22 +1,20 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
   const normalizedVNode = normalizeVNode(vNode);
-  // const existingElement = container.firstChild;
+  const existingElement = container.firstChild;
 
-  // if (!existingElement) {
-  const element = createElement(normalizedVNode);
-  if (element && element !== "") {
-    container.innerHTML = "";
-    container.appendChild(element);
+  if (!existingElement) {
+    const element = createElement(normalizedVNode);
+    if (element && element !== "") {
+      container.appendChild(element);
+    }
+  } else {
+    updateElement(existingElement, normalizedVNode, existingElement._vNode);
   }
-  // } else {
-  // 업데이트 렌더링: diff 알고리즘 사용
-  // updateElement(existingElement, normalizedVNode, existingElement._vNode);
-  // }
 
   if (!container._eventListenersSetup) {
     setupEventListeners(container);
